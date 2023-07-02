c32b47f71a7041fbae5886bb53b6f6cb39f71e1b
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-07-02 12:08:19 +0300

    1

--- controllers/contacts/getContacts.js
+++
@@ -1,8 +0,0 @@
-const { Contact } = require("../../models/contact");
-const { ctrlWrapper } = require("../../helpers");
-
-const getContacts = async (req, res) => {
-  const result = await Contact.find();
-  res.json(result);
-};
-module.exports = { getContacts: ctrlWrapper(getContacts) };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const ctrl = require("../../controllers/contacts");
+const ctrl = require("../../controllers/contacts/contacts");
 const { validateBody, isValidId } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
