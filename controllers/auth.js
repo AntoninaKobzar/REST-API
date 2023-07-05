ef2393c9c66701bd0803f9919bbde90e016c9490
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-07-05 10:42:48 +0300

    1

---
+++ controllers/auth.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email is already in use");
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    email: newUser.email,
+    name: newUser.name,
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or passwors invalid");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or passwors invalid");
+  }
+  const payload = {
+    id: "64a15f27bbc3d22cd385a05c",
+  };
+  const token = "fdshffsdgh.e4rq2tas.dr13541";
+  res.json({
+    token,
+  });
+};
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+};
