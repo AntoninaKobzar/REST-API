f8ad6ab63729a892e52bf2d293f6667e29eb7f9d
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-07-14 16:09:20 +0300

    1

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// const jwt = require("jsonwebtoken");
-// require("dotenv").config();
-// const { SECRET_KEY } = process.env;
-// const payload = {
-//   id: "64a15f27bbc3d22cd385a05c",
-// };
-// const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-// // console.log(token);
-// const decodeToken = jwt.decode(token);
-// // console.log(decodeToken);
-// try {
-//   const { id } = jwt.verify(token, SECRET_KEY);
-//   console.log(id);
-//   const invalidToken =
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YTE1ZjI3YmJjM2QyMmNkMzg1YTA1YyIsImlhdCI6MTY4ODMxMjEyNSwiZXhwIjoxNjg4Mzk0OTI1fQ.GpqV1qcQmtg035UT0rumeuAA7qoCCbZJ8Ha_GezYhF4";
-//   const result = jwt.verify(invalidToken, SECRET_KEY);
-// } catch (error) {
-//   console.log(error.message);
-// }
-
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
